6fa06ae796057fd02da89fd3d622eff5fa31a1cf
ruslanchampion <ruslan.timbakov@gmail.com>
2020-09-13 22:26:09 +0800

    feat: add desicion to transformArr task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  let copy = Array.apply(null, arr);
+  let commands = [
+    "--discard-prev",
+    "--double-prev",
+    "--double-next",
+    "--discard-next",]
+  copy.map((el, i, array) => {
+    switch (el) {
+    case "--discard-prev" :
+      i > 0 ? (array[i - 1] = undefined) : false;
+      break;
+    case  "--double-prev":
+      i > 0 ? (array[i] = array[i - 1]) : false;
+      break;
+    case  "--double-next":
+      i < array.length - 1 ? (array[i] = array[i + 1]) : false;
+      break;
+    case "--discard-next":
+      i < array.length - 1 ? (array[i + 1] = undefined) : false;
+      break;
+    }
+  });
+  return copy.filter((el) => el != undefined && commands.indexOf(el) == -1);
+}
