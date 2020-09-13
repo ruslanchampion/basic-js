442d98f0c831390839e7fa1841dcb7873428bb79
ruslanchampion <ruslan.timbakov@gmail.com>
2020-09-13 17:52:54 +0800

    feat: add decision to count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let count = 0;
+  for(let i=0; i<backyard.length; i++){
+      for (let str of backyard[i]) {
+          if (str=='^^'){
+              count++;
+          }
+      }    
+  }
+  return count;
 };
