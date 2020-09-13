9eaf3abdd334bc4db7be1e4c9e158ddc83c267b5
ruslanchampion <ruslan.timbakov@gmail.com>
2020-09-13 18:41:37 +0800

    feat: add desicion to dreamTeam task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if(!Array.isArray(members) || members.length === 0) return false;
+  return members.filter(item => (typeof item === 'string' && item.length !== 0)).map(item => {
+    return item.replace(/\s/g, '')
+  }).map(item => item[0].toUpperCase()).sort().join('');
 };
