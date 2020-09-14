2da054bad36a3c06c9088ec17d2521993e3f82ac
ruslanchampion <ruslan.timbakov@gmail.com>
2020-09-14 18:53:13 +0800

    feat: add desicion to recursive task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr, count = 1, res = []) {
+    res.push(count);
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        this.calculateDepth(arr[i], count + 1, res)
+      }
+    }
+    return res.sort((a, b) => b - a)[0];
   }
 };
